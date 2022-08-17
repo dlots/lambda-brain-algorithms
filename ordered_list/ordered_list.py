9a5668819f71272b74608942cca82356ee2b2cfe
dlots <dlots42@gmail.com>
2022-08-18 00:16:19 +0300

    Task 7: ordered list (+ tests)

---
+++ ordered_list/ordered_list.py
@@ -0,0 +1,115 @@
+class Node:
+    def __init__(self, v):
+        self.value = v
+        self.prev = None
+        self.next = None
+
+
+class OrderedList:
+    def __init__(self, asc):
+        self.head = None
+        self.tail = None
+        self.__ascending = asc
+        self.__length = 0
+        self.find_steps = 0
+
+    def compare(self, v1, v2):
+        result = -1
+        if v1 == v2:
+            result = 0
+        elif v1 > v2:
+            result = 1
+        return result
+
+    def add(self, value):
+        new_node = Node(value)
+        self.__length += 1
+        if self.__length == 1:
+            self.head = new_node
+            self.tail = new_node
+            return
+        node = self.head
+        while node is not None:
+            comparison_result = self.compare(value, node.value)
+            if (self.__ascending and comparison_result == 1) or (not self.__ascending and comparison_result == -1):
+                node = node.next
+                continue
+            if node.prev is not None:
+                new_node.prev = node.prev
+                new_node.prev.next = new_node
+            new_node.next = node
+            node.prev = new_node
+            if self.head == node:
+                self.head = new_node
+            return
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+
+    def find(self, val):
+        self.find_steps = 0
+        node = self.head
+        while node is not None:
+            self.find_steps += 1
+            comparison_result = self.compare(val, node.value)
+            if (self.__ascending and comparison_result == 1) or (not self.__ascending and comparison_result == -1):
+                node = node.next
+                continue
+            elif (self.__ascending and comparison_result == -1) or (not self.__ascending and comparison_result == 1):
+                return None
+            break
+        return node
+
+    def delete(self, val):
+        node = self.head
+        while node is not None:
+            comparison_result = self.compare(val, node.value)
+            if (self.__ascending and comparison_result == 1) or (not self.__ascending and comparison_result == -1):
+                node = node.next
+                continue
+            elif (self.__ascending and comparison_result == -1) or (not self.__ascending and comparison_result == 1):
+                return
+            if node.value != val:
+                node = node.next
+                continue
+            self.__length -= 1
+            if node is self.tail and node is self.head:
+                self.tail = None
+                self.head = None
+            elif node is self.tail:
+                self.tail = node.prev
+                node.prev.next = node.next
+            elif node is self.head:
+                self.head = node.next
+                self.head.prev = None
+            else:
+                node.prev.next = node.next
+                node.next.prev = node.prev
+            return
+
+    def clean(self, asc):
+        self.__ascending = asc
+        self.head = None
+        self.tail = None
+        self.__length = 0
+        self.find_steps = 0
+
+    def len(self):
+        return self.__length
+
+    def get_all(self):
+        r = []
+        node = self.head
+        while node is not None:
+            r.append(node)
+            node = node.next
+        return r
+
+
+class OrderedStringList(OrderedList):
+    def __init__(self, asc):
+        super(OrderedStringList, self).__init__(asc)
+
+    def compare(self, v1, v2):
+        super(OrderedStringList, self).compare(v1.strip(), v2.strip())
+        return 0
---
+++ ordered_list/ordered_list_tests.py
@@ -0,0 +1,83 @@
+import unittest
+from ordered_list import OrderedList
+
+
+class TestOrderedList(unittest.TestCase):
+    def test_add_ascending(self):
+        o_list = OrderedList(True)
+        nums = [23, 532, 13, 36, 12, 4, 16, 13, 1, 24, 5612, 412, 5]
+        for i, num in enumerate(nums):
+            o_list.add(num)
+            self.assertEqual(o_list.len(), i + 1)
+            to_list = [node.value for node in o_list.get_all()]
+            self.assertTrue(num in to_list)
+            for j in range(o_list.len() - 1):
+                self.assertTrue(to_list[j] <= to_list[j + 1])
+
+    def test_add_descending(self):
+        o_list = OrderedList(False)
+        nums = [23, 532, 13, 36, 12, 4, 16, 13, 1, 24, 5612, 412, 5]
+        for i, num in enumerate(nums):
+            o_list.add(num)
+            self.assertEqual(o_list.len(), i + 1)
+            to_list = [node.value for node in o_list.get_all()]
+            self.assertTrue(num in to_list)
+            for j in range(o_list.len() - 1):
+                self.assertTrue(to_list[j] >= to_list[j + 1])
+
+    def test_delete_ascending(self):
+        o_list = OrderedList(True)
+        nums = [23, 532, 36, 12, 4, 16, 13, 1, 24, 5612, 412, 5]
+        for i, num in enumerate(nums):
+            o_list.add(num)
+        self.assertEqual(o_list.len(), len(nums))
+        for i, num in enumerate(nums):
+            o_list.delete(num)
+            self.assertEqual(o_list.len(), len(nums) - i - 1)
+            to_list = [node.value for node in o_list.get_all()]
+            self.assertTrue(num not in to_list)
+            for j in range(o_list.len() - 1):
+                self.assertTrue(to_list[j] <= to_list[j + 1])
+
+    def test_delete_descending(self):
+        o_list = OrderedList(False)
+        nums = [23, 532, 36, 12, 4, 16, 13, 1, 24, 5612, 412, 5]
+        for i, num in enumerate(nums):
+            o_list.add(num)
+        self.assertEqual(o_list.len(), len(nums))
+        for i, num in enumerate(nums):
+            o_list.delete(num)
+            self.assertEqual(o_list.len(), len(nums) - i - 1)
+            to_list = [node.value for node in o_list.get_all()]
+            self.assertTrue(num not in to_list)
+            for j in range(o_list.len() - 1):
+                self.assertTrue(to_list[j] >= to_list[j + 1])
+
+    def test_find_ascending(self):
+        o_list = OrderedList(True)
+        nums = [23, 532, 36, 12, 4, 16, 13, 1, 24, 5612, 412, 5]
+        for i, num in enumerate(nums):
+            o_list.add(num)
+        self.assertIsNotNone(o_list.find(12))
+        self.assertEqual(o_list.find(6), None)
+        self.assertEqual(o_list.find_steps, 4)
+
+    def test_find_descending(self):
+        o_list = OrderedList(False)
+        nums = [23, 532, 36, 12, 4, 16, 13, 1, 24, 5612, 412, 5]
+        for i, num in enumerate(nums):
+            o_list.add(num)
+        self.assertIsNotNone(o_list.find(12))
+        self.assertEqual(o_list.find(6), None)
+        self.assertEqual(o_list.find_steps, 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    # o_list = OrderedList(False)
+    # nums = [23, 532, 13, 36, 12, 4, 16, 13, 1, 24, 5612, 412, 5]
+    # for num in nums:
+    #     o_list.add(num)
+    # for num in nums:
+    #     o_list.delete(num)
+    #     print([node.value for node in o_list.get_all()])
