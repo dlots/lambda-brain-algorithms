52cea78bcf4f7c26fb0c889548dd1e9357adade9
dlots <dlots42@gmail.com>
2022-10-08 17:04:21 +0300

    array binary search tree

---
+++ array_binary_search_tree/array_binary_search_tree.py
@@ -0,0 +1,52 @@
+def calculate_tree_size(depth):
+    size = 0
+    for x in range(depth):
+        size += 2 ** x
+    return size
+
+
+def get_parent_index(current_index):
+    return (current_index - 1) / 2
+
+
+def get_left_child_index(current_index):
+    return 2 * current_index + 1
+
+
+def get_right_child_index(current_index):
+    return 2 * current_index + 2
+
+
+class aBST:
+
+    def __init__(self, depth):
+        tree_size = calculate_tree_size(depth)
+        self.Tree = [None] * tree_size
+
+    def find_key_index_recursively(self, key, current_index):
+        if current_index >= len(self.Tree):
+            return None
+        current_key = self.Tree[current_index]
+        if current_key is None:
+            return -current_index
+        if key == current_key:
+            return current_index
+        next_index = None
+        if key < current_key:
+            next_index = get_left_child_index(current_index)
+        if key > current_key:
+            next_index = get_right_child_index(current_index)
+        return self.find_key_index_recursively(key, next_index)
+
+    def FindKeyIndex(self, key):
+        return self.find_key_index_recursively(key, 0)
+
+    def AddKey(self, key):
+        key_index = self.FindKeyIndex(key)
+        if key_index is None:
+            return None
+        if key_index < 0 or (key_index == 0 and self.Tree[0] is None):
+            key_index = -key_index
+            self.Tree[key_index] = key
+            return key_index
+        return key_index
---
+++ array_binary_search_tree/array_binary_search_tree_tests.py
@@ -0,0 +1,44 @@
+import unittest
+from random import shuffle
+from array_binary_search_tree import aBST, calculate_tree_size
+
+
+class TestArrayBinarySearchTree(unittest.TestCase):
+    def test_calculate_tree_size(self):
+        self.assertEqual(calculate_tree_size(1), 1)
+        self.assertEqual(calculate_tree_size(2), 3)
+        self.assertEqual(calculate_tree_size(3), 7)
+        self.assertEqual(calculate_tree_size(4), 15)
+        self.assertEqual(calculate_tree_size(5), 31)
+
+    def test_find_in_empty_tree(self):
+        tree = aBST(5)
+        for key in range(100):
+            index = tree.FindKeyIndex(key)
+            self.assertEqual(index, 0)
+            self.assertIsNone(tree.Tree[index])
+
+    def test_make_full_tree(self):
+        depth = 3
+        tree = aBST(depth)
+        keys = [10, 5, 15, 2, 6, 12, 17]
+        for key in keys:
+            index = tree.AddKey(key)
+            self.assertGreaterEqual(index, 0)
+            self.assertEqual(tree.Tree[index], key)
+            self.assertEqual(tree.FindKeyIndex(key), index)
+        for key in keys:
+            self.assertGreaterEqual(tree.FindKeyIndex(key), 0)
+
+    def test_cant_exceed_depth_tree_is_not_full(self):
+        depth = 3
+        tree = aBST(depth)
+        keys = [10, 5, 2, 6]
+        for key in keys:
+            tree.AddKey(key)
+        self.assertIsNone(tree.AddKey(1))
+        self.assertIsNone(tree.AddKey(7))
+
+
+if __name__ == '__main__':
+    unittest.main()
